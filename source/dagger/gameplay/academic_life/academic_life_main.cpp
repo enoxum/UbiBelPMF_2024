dee90253567d61e47ac7f88a64c57eddb6b5abd6
StefanJevtic <stefanjevtic10@yahoo.com>
2024-05-06 22:05:35 +0200

    Created game directory

---
+++ source/dagger/gameplay/academic_life/academic_life_main.cpp
@@ -0,0 +1,20 @@
+#include "academic_life_main.h"
+
+#include "core/core.h"
+#include "core/engine.h"
+#include "core/input/inputs.h"
+#include "core/graphics/sprite.h"
+#include "core/graphics/animation.h"
+#include "core/graphics/shaders.h"
+#include "core/graphics/window.h"
+#include "core/game/transforms.h"
+#include "core/graphics/sprite_render.h"
+#include "core/graphics/textures.h"
+#include "core/graphics/animations.h"
+#include "core/graphics/gui.h"
+#include "tools/diagnostics.h"
+
+#include "gameplay/common/simple_collisions.h"
+
+using namespace dagger;
+using namespace academic_life;
