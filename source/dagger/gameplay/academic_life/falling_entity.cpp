724c3bf177a45d7a1eb86024d93fe7d9d3116f3a
StefanJevtic <stefanjevtic10@yahoo.com>
2024-05-14 22:51:57 +0200

    refactor: implemented template class method

--- source/dagger/gameplay/academic_life/falling_entity.cpp
+++ source/dagger/gameplay/academic_life/falling_entity.cpp
@@ -3,11 +3,8 @@
 #include "core/engine.h"
 #include "core/game/transforms.h"
 
-#include "gameplay/academic_life/academic_life_logic.h"
-
 using namespace dagger;
 using namespace academic_life;
-
 
 void FallingEntitySystem::Run()
 {
@@ -18,55 +15,11 @@ void FallingEntitySystem::Run()
     }
 
     Float32 boarderY = fieldSettings.GetYBoarder();
-    static float mul = 4;
+    float mul = 4;
     static int fallingEntityCounter = 0;
     float entitySpeed = fieldSettings.fieldTileSize * mul;
 
-    auto view = Engine::Registry().view<Transform, FallingEntity>();
-    for (auto entity : view)
-    {
-        auto& transform = view.get<Transform>(entity);
-        auto& falling_entity = view.get<FallingEntity>(entity);
-        falling_entity.speed = entitySpeed;
+    ProcessEntity<FallingEntity>(boarderY, mul, entitySpeed, fallingEntityCounter, fieldSettings, false);
+    ProcessEntity<FallingText>(boarderY, mul, entitySpeed, fallingEntityCounter, fieldSettings, true);
 
-        transform.position.y -= falling_entity.speed * Engine::DeltaTime();
-
-        // Umesto da se vrati gore kada padne na zemlju, bolje je da se kreira novi objekat (drugog tipa)
-        if (transform.position.y < -boarderY)
-        {
-            transform.position.y = boarderY;
-            fallingEntityCounter += 1;
-            if (fallingEntityCounter % 3 == 0) {
-                mul += 0.4;
-                entitySpeed = fieldSettings.fieldTileSize * mul;
-            }
-        }
-    }
-
-    mul = 4;
-    fallingEntityCounter = 0;
-    entitySpeed = fieldSettings.fieldTileSize * mul;
-    auto view2 = Engine::Registry().view<Transform, FallingText>();
-    for (auto entity : view2)
-    {
-        auto& transform = view2.get<Transform>(entity);
-        auto& falling_entity = view2.get<FallingText>(entity);
-        falling_entity.speed = entitySpeed;
-        auto& text = falling_entity.text;
-
-        transform.position.y -= falling_entity.speed * Engine::DeltaTime();
-
-        // Umesto da se vrati gore kada padne na zemlju, bolje je da se kreira novi objekat (drugog tipa)
-        if (transform.position.y < -boarderY)
-        {
-            transform.position.y = boarderY;
-            fallingEntityCounter += 1;
-            if (fallingEntityCounter % 3 == 0) {
-                mul += 0.4;
-                entitySpeed = fieldSettings.fieldTileSize * mul;
-            }
-        }
-
-        text.Set("pixel-font", "jednacina", transform.position);
-    }
 }
--- source/dagger/gameplay/academic_life/falling_entity.h
+++ source/dagger/gameplay/academic_life/falling_entity.h
@@ -4,20 +4,34 @@
 #include "core/system.h"
 #include "core/core.h"
 
+#include "gameplay/academic_life/academic_life_logic.h"
+
 using namespace dagger;
 
 namespace academic_life
 {
-    struct FallingEntity
+    struct FallingEntityBase
     {
         Float32 speed;
+        virtual void SetText(const String& font, const String& text, const Vector3& position) = 0;
     };
 
-    struct FallingText
+    struct FallingEntity : public FallingEntityBase
     {
-        Float32 speed;
-        Text text;
+        void SetText(const String& font, const String& text, const Vector3& position) override
+        {   }
     };
+
+    struct FallingText : public FallingEntityBase
+    {
+        Text text;
+        void SetText(const String& font, const String& text, const Vector3& position) override
+        {
+            this->text.Set(font, text, position);
+        }
+    };
+
+
 
     class FallingEntitySystem : public System
     {
@@ -25,5 +39,35 @@ namespace academic_life
         inline String SystemName() { return "Falling Entity's System"; }
 
         void Run() override;
+
+        template<typename T>
+        void ProcessEntity(Float32 boarderY, float mul, float entitySpeed, int& fallingEntityCounter,
+            AcademicLifeFieldSettings fieldSettings, bool isText) 
+        {
+            auto view = Engine::Registry().view<Transform, T>();
+            for (auto entity : view)
+            {
+                auto& transform = view.get<Transform>(entity);
+                auto& falling_entity = view.get<T>(entity);
+                falling_entity.speed = entitySpeed;
+
+                transform.position.y -= falling_entity.speed * Engine::DeltaTime();
+
+                // Umesto da se vrati gore kada padne na zemlju, bolje je da se kreira novi objekat (drugog tipa)
+                if (transform.position.y < -boarderY)
+                {
+                    transform.position.y = boarderY;
+                    fallingEntityCounter += 1;
+                    if (fallingEntityCounter % 3 == 0) 
+                    {
+                        mul += 0.4;
+                        entitySpeed = fieldSettings.fieldTileSize * mul;
+                    }
+                }
+
+                if (isText) 
+                    falling_entity.SetText("pixel-font", "jednacina", transform.position);
+            }
+        }
     };
 }
