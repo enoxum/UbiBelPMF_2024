d77a6aac512d024ab8573a99f6144fb072fd8db7
StefanJevtic <stefanjevtic10@yahoo.com>
2024-05-15 20:02:38 +0200

    feature: Implemented Equation class

--- source/dagger/gameplay/academic_life/falling_entity.cpp
+++ source/dagger/gameplay/academic_life/falling_entity.cpp
@@ -6,6 +6,74 @@
 using namespace dagger;
 using namespace academic_life;
 
+std::string Equation::random_operator_simple() const
+{
+    std::vector<std::string> operators = { "+", "-" };
+    return operators[rand() % 2];
+}
+
+std::string Equation::random_operator_medium() const
+{
+    std::vector<std::string> operators = { "+", "*", "/" };
+    return operators[rand() % 3];
+}
+
+std::string Equation::random_operator_hard() const
+{
+    std::vector<std::string> operators = { "-", "*", "^" };
+    return operators[rand() % 3];
+}
+
+int Equation::random_terminal() const
+{
+    return rand() % (m_ub - m_lb + 1) + m_lb;
+}
+
+std::string Equation::to_string(const std::vector<std::string>& code)
+{
+    return "(" + code[3] + " " + code[1] + " " + code[4] + ") " +
+        code[0] + " (" + code[5] + " " + code[2] + " " + code[6] + ")";
+}
+
+std::string Equation::to_equation(const std::string& expression) const
+{
+    return "X = " + expression;
+}
+
+double Equation::calculate(const std::string& expression) {
+    /*exprtk::symbol_table<double> symbol_table;
+    symbol_table.add_constants();
+
+    exprtk::expression<double> expr;
+    expr.register_symbol_table(symbol_table);
+
+    exprtk::parser<double> parser;
+    if (parser.compile(expression, expr)) {
+        return expr.value();
+    }
+    else {
+        throw std::runtime_error("Failed to parse equation");
+    }
+    */
+    return 0.f;
+
+}
+
+const std::vector<std::string>& Equation::get_code_simple() const
+{
+    return m_code_simple;
+}
+
+const std::vector<std::string>& Equation::get_code_medium() const
+{
+    return m_code_medium;
+}
+
+const std::vector<std::string>& Equation::get_code_hard() const
+{
+    return m_code_hard;
+}
+
 void FallingEntitySystem::Run()
 {
     AcademicLifeFieldSettings fieldSettings;
--- source/dagger/gameplay/academic_life/falling_entity.h
+++ source/dagger/gameplay/academic_life/falling_entity.h
@@ -10,25 +10,59 @@ using namespace dagger;
 
 namespace academic_life
 {
-    struct FallingEntityBase
+    struct FallingEntity
     {
-        Float32 speed;
-        virtual void SetText(const String& font, const String& text, const Vector3& position) = 0;
+        float speed;
+        Text text;  // does nothing
     };
 
-    struct FallingEntity : public FallingEntityBase
+    struct FallingText
     {
-        void SetText(const String& font, const String& text, const Vector3& position) override
-        {   }
-    };
-
-    struct FallingText : public FallingEntityBase
-    {
+        float speed;
         Text text;
-        void SetText(const String& font, const String& text, const Vector3& position) override
+    };
+
+    class Equation {
+    public:
+        Equation::Equation(const unsigned num_operators, const unsigned num_operands, const int lb, const int ub)
+            : m_num_operators(num_operators), m_num_operands(num_operands), m_lb(lb), m_ub(ub)
         {
-            this->text.Set(font, text, position);
+            initialize_code(m_code_simple, &Equation::random_operator_simple);
+            initialize_code(m_code_medium, &Equation::random_operator_medium);
+            initialize_code(m_code_hard, &Equation::random_operator_hard);
         }
+
+        template<typename T>
+        void initialize_code(T& code, std::string(Equation::* random_operator)() const)
+        {
+            for (int i = 0; i < m_num_operators; i++) {
+                code.push_back((this->*random_operator)());
+            }
+            for (int i = 0; i < m_num_operands; i++) {
+                code.push_back(std::to_string(random_terminal()));
+            }
+        }
+
+        std::string random_operator_simple() const;
+        std::string random_operator_medium() const;
+        std::string random_operator_hard() const;
+        int random_terminal() const;
+
+        std::string to_string(const std::vector<std::string>& code);
+        std::string to_equation(const std::string& expression) const;
+        double calculate(const std::string& expression);
+
+        const std::vector<std::string>& get_code_simple() const;
+        const std::vector<std::string>& get_code_medium() const;
+        const std::vector<std::string>& get_code_hard() const;
+
+    private:
+        std::vector<std::string> m_code_simple;
+        std::vector<std::string> m_code_medium;
+        std::vector<std::string> m_code_hard;
+
+        unsigned m_num_operators, m_num_operands;
+        int m_lb, m_ub;
     };
 
     template<typename T>
@@ -57,7 +91,7 @@ namespace academic_life
             }
 
             if (isText)
-                falling_entity.SetText("pixel-font", "jednacina", transform.position);
+                falling_entity.text.Set("pixel-font", falling_entity.text.message, transform.position);
         }
     }
 
