2e6272463e3a016722885b59d5cbe76cef4a5361
Andrijana0502 <ivkovich.ana@gmail.com>
2024-05-21 02:39:22 +0200

    new commit

--- source/dagger/main.cpp
+++ source/dagger/main.cpp
@@ -6,6 +6,7 @@
 #include "gameplay/team_game/team_game_main.h"
 #include "gameplay/tiles_example/tiles_example_main.h"
 #include "gameplay/editor/editor_main.h"
+#include "gameplay/red_game/red_snake_main.h"
 //Test test
 // test test vladan
 
@@ -16,6 +17,8 @@ int main(int argc_, char** argv_)
 //	return engine.Run<team_game::TeamGame>();
 //	return engine.Run<ping_pong::PingPongGame>();
 //	return engine.Run<racing_game::RacingGame>();
-	return engine.Run<platformer::Platformer>();
+	//return engine.Run<platformer::Platformer>();
+	return engine.Run<red_snake::RedSnakeGame>();
+
 //	return engine.Run<editor::EditorTestGame>();
 }
