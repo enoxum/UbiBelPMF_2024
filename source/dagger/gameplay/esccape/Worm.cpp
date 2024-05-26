40b00eca767436ab95ff46a7505d8375b080c769
Matija <matija0201@gmail.com>
2024-05-26 15:03:42 +0200

    add more worms and introduce randomness for worm spawns

--- source/dagger/gameplay/esccape/Worm.cpp
+++ source/dagger/gameplay/esccape/Worm.cpp
@@ -18,13 +18,6 @@ void esccape::WormSystem::Run()
         //sprite.scale.y = -std::abs(sprite.scale.y);
         //auto& col = view.get<SimpleCollision>(entity);
         if (worm.spawnEdge == 0 && t.position.y < -worm.startingYTop - 10) {
-            printf("Usao u if\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-            //std::random_device rd;
-            //std::mt19937 gen(rd());
-            //std::uniform_int_distribution<> dis(-(worm.startingPos.y), worm.startingPos.y);
-            //int x_pos = dis(gen);
-            //t.position.x = x_pos;
-            //t.position.y = worm.startingPos.y;
             int randomEdge = rand() % 4;
             int pos = generateSpawnPoint(randomEdge, worm);
             setPosition(randomEdge, pos, t, worm, sprite);
@@ -56,13 +49,13 @@ void esccape::WormSystem::Run()
 int esccape::generateSpawnPoint(int edge, Worm& worm) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    int randomEdge = rand() % 4;
     int x_pos, y_pos;
     std::uniform_int_distribution<> dis0(worm.minimumX, worm.maximumX);
     std::uniform_int_distribution<> dis1(worm.minimumY, worm.maximumY);
     std::uniform_int_distribution<> dis2(worm.minimumX, worm.maximumX);
     std::uniform_int_distribution<> dis3(worm.minimumY, worm.maximumY);
-    switch (randomEdge) {
+
+    switch (edge) {
         case 0:
             x_pos = dis0(gen);
             return x_pos;
@@ -73,38 +66,43 @@ int esccape::generateSpawnPoint(int edge, Worm& worm) {
             x_pos = dis2(gen);
             return x_pos;
         case 3:
-            x_pos = dis3(gen);
-            return x_pos;
+            y_pos = dis3(gen);
+            return y_pos;
     }
     
     return -1;
 }
 
 void esccape::setPosition(int edge, int pos, Transform& t, Worm& worm, Sprite& sprite) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> disDistance(300, 1500);
+    std::uniform_int_distribution<> disSpeed(200, 400);
+    int speed = disSpeed(gen);
     switch (edge) {
         case 0:
             t.position.x = pos;
-            t.position.y = worm.startingYTop;
+            t.position.y = worm.startingYTop + disDistance(gen);
             sprite.rotation = 0;
-            worm.speed = { 0, -100, 0 };
+            worm.speed = { 0, -speed, 0 };
             break;
         case 1:
-            t.position.x = worm.startingXRight;
+            t.position.x = worm.startingXRight + disDistance(gen);
             t.position.y = pos;
             sprite.rotation = 270;
-            worm.speed = { -100, 0, 0 };
+            worm.speed = { -speed, 0, 0 };
             break;
         case 2:
             t.position.x = pos;
-            t.position.y = worm.startingYBottom;
+            t.position.y = worm.startingYBottom - disDistance(gen);
             sprite.rotation = 180;
-            worm.speed = { 0, 100, 0 };
+            worm.speed = { 0, speed, 0 };
             break;
         case 3:
-            t.position.x = worm.startingXLeft;
+            t.position.x = worm.startingXLeft - disDistance(gen);
             t.position.y = pos;
             sprite.rotation = 90;
-            worm.speed = { 100, 0, 0 };
+            worm.speed = { speed, 0, 0 };
             break;
     }
 }
